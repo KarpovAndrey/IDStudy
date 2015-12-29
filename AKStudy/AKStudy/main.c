b95efbd874c00b0681e2c4f24e035490ad83e188
Andrey Karpov <a_karp@mail.ru>
2015-12-29 16:42:08 +0200

    start project AKStudy

---
+++ AKStudy/AKStudy/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AKStudy
+//
+//  Created by Admin on 29.12.15.
+//  Copyright © 2015 Admin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
