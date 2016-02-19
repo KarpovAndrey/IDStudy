ee32ffeb13b5f109381ff11d978bf63e071430ad
Andrey Karpov <a_karp@mail.ru>
2016-02-20 00:32:45 +0200

    create AKCreature

---
+++ AKStudy/ObjC/Test/AKCreatureTest/AKCreatureTest.m
@@ -0,0 +1,13 @@
+//
+//  AKCreatureTest.m
+//  AKStudy
+//
+//  Created by Admin on 19.02.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "AKCreatureTest.h"
+
+@implementation AKCreatureTest
+
+@end
