4bb6f552953af2a3a385e36b27d38a31819c4bcc
Andrey Karpov <a_karp@mail.ru>
2016-03-15 18:39:11 +0200

    AKObserver added

---
+++ AKStudy/ObjC/SRC/AKObserver/AKObserver.h
@@ -0,0 +1,24 @@
+//
+//  AKObserver.h
+//  AKStudy
+//
+//  Created by Admin on 15.03.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AKObserver : NSObject
+@property (nonatomic, readonly) NSUInteger state;
+@property (nonatomic, readonly) NSArray    *observers;
+
+- (void)addObserver:(id)observer;
+- (void)removeObserver:(id)observer;
+
+// This method is intended for subclassing
+- (SEL)selectorForState:(NSUInteger)state;
+
+- (void)notifyObservers;
+- (void)notifyObserversWithSelector:(SEL)selector;
+
+@end
---
+++ AKStudy/ObjC/SRC/AKObserver/AKObserver.m
@@ -0,0 +1,85 @@
+//
+//  AKObserver.m
+//  AKStudy
+//
+//  Created by Admin on 15.03.16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "AKObserver.h"
+
+@interface AKObserver ()
+@property  (nonatomic, assign) NSMutableArray *mutableObservers;
+@property  (nonatomic, assign) NSUInteger     stateObserver;
+
+@end
+
+@implementation AKObserver
+
+@dynamic observers;
+@dynamic state;
+
+#pragma mark -
+#pragma mark Initializations & Deallocations
+
+- (void)dealloc {
+    self.mutableObservers = nil;
+    
+    [super dealloc];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.mutableObservers = [NSMutableArray array];
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSArray *)observers {
+    return [self.mutableObservers copy];
+}
+
+- (void)setState:(NSUInteger)state {
+    if (_stateObserver != state) {
+        _stateObserver = state;
+    
+        [self notifyObservers];
+    }
+}
+
+- (NSUInteger)state {
+    return self.stateObserver;
+}
+
+#pragma mark -
+#pragma mark Public
+
+- (void)addObserver:(id)observer {
+    [self.mutableObservers addObject:observer];
+}
+
+- (void)removeObserver:(id)observer {
+    [self.mutableObservers removeObject:observer];
+}
+
+- (SEL)selectorForState:(NSUInteger)state {
+    return nil;
+}
+
+- (void)notifyObservers {
+    [self notifyObserversWithSelector:[self selectorForState:self.state]];
+}
+
+- (void)notifyObserversWithSelector:(SEL)selector {
+    for (id observer in self.observers) {
+        if ([observer respondsToSelector:selector]) {
+            [observer performSelector:selector];
+        }
+    }
+}
+
+@end
